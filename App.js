5f9fbaf3c969b5cf6e7a64523e61e729bfbdaa69
rizwan.atta <rizwan.atta@washmen.com>
2025-07-23 10:05:37 +0500

    session 19 working eas setup

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 import "./global.css";
+import { useFonts } from "expo-font";
 
 import RootNavigation from "./src/navigation/rootNavigation";
 
 export default function App() {
+  const [loaded] = useFonts({
+    Bitcount: require("./assets/fonts/bitcountfont.ttf"),
+  });
+
+  if (!loaded) {
+    return null;
+  }
+
   return <RootNavigation />;
 }
--- src/screens/animationPractice.js
+++ src/screens/animationPractice.js
@@ -1,75 +1,18 @@
-import { View, Image, Button } from "react-native";
-import Animated, {
-  useSharedValue,
-  withTiming,
-  Easing,
-  useAnimatedStyle,
-  withRepeat,
-  withSequence,
-} from "react-native-reanimated";
-import { MotiView } from "moti";
-
-const ANGLE = 10;
-const TIME = 100;
-const EASING = Easing.elastic(1.5);
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function AnimationPractice() {
-  const width = useSharedValue(200);
-
-  const rotation = useSharedValue(0);
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ rotateZ: `${rotation.value}deg` }],
-  }));
-
-  const handlePress = () => {
-    rotation.value = withSequence(
-      // deviate left to start from -ANGLE
-      withTiming(-ANGLE, { duration: TIME / 2, easing: EASING }),
-      // wobble between -ANGLE and ANGLE 7 times
-      withRepeat(
-        withTiming(ANGLE, {
-          duration: TIME,
-          easing: EASING,
-        }),
-        7,
-        true
-      ),
-      // go back to 0 at the end
-      withTiming(0, { duration: TIME / 2, easing: EASING })
-    );
-  };
-
   return (
     <View className="flex-1 items-center justify-center">
-      <Animated.View
-        style={[
-          {
-            width,
-            height: 300,
-          },
-          animatedStyle,
-        ]}
-      >
-        <Image
-          className="w-[200px] h-[200px]"
-          source={{
-            uri: "https://cdn.pixabay.com/photo/2016/10/15/18/59/coffee-cup-1743338_1280.png",
+      <TouchableOpacity>
+        <Text
+          style={{
+            fontFamily: "Bitcount",
+            fontSize: 80,
           }}
-        />
-      </Animated.View>
-
-      <MotiView
-        className="w-[200px] h-[200px] rounded-full bg-red-500"
-        from={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          type: "timing",
-          duration: 3500,
-        }}
-      />
-
-      <Button onPress={handlePress} title="Click me" />
+        >
+          Press me
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 }
